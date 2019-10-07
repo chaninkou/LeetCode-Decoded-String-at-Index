7fee90a428a5efc648be2a876f17c6630433e6cf
chaninkou <michaelkoulove@yahoo.com>
2019-10-07 16:58:46 -0700

    Ok problem, clean solution with getting the total length and go back.

---
+++ LeetCode-Decoded-String-at-Index/src/decoded/GetDecodeLetterAtIndexFunction.java
@@ -0,0 +1,48 @@
+package decoded;
+
+public class GetDecodeLetterAtIndexFunction {
+    public String decodeAtIndex(String S, int K) {
+        long currentLength = 0;
+        
+        char letter = 0;
+        
+        // Count the total length
+        for(int i = 0; i < S.length(); i++){
+            letter = S.charAt(i);
+            
+            // If current index is a digit, then multiply the number of times it appear
+            if(Character.isDigit(letter)){
+                // letter - '0' to get the integer value
+                currentLength = currentLength * (letter - '0');
+            } else { // Current index is a letter
+                // Update the length increase by 1
+                currentLength += 1;
+            }
+        }
+        
+        // Start from the end now, whenever we see digit, divide current length with that digit
+        for(int end = S.length() - 1; end >= 0; end--){
+            letter = S.charAt(end);
+            
+            // If its a digit, now we divide by that digit
+            if(Character.isDigit(letter)){
+                currentLength = currentLength / (letter - '0');
+                
+                // Update K since we want to know which exactly index is the one we are looking for
+                K %= currentLength;
+            } else {
+                // When its not digit, check rather if K is 0 or currentLength is already at K
+                if(K == 0 || K == currentLength){
+                    // "" so the character will convert to string instead
+                    return "" + letter;
+                }
+                
+                // Update the length by decreasing by 1
+                currentLength -= 1;
+            }
+        }
+        
+        // If it gets here, the input didn't meet the condition
+        return null;
+    }         
+}
--- LeetCode-Decoded-String-at-Index/src/decoded/Main.java
+++ LeetCode-Decoded-String-at-Index/src/decoded/Main.java
@@ -6,43 +6,12 @@ public class Main {
 		
 		int K = 10;
 		
-		System.out.println("Input: " + S);
+		System.out.println("Input: " + S + " K: " + K );
 		
-		System.out.println("Solution: " + decodeAtIndex(S, K));
+		GetDecodeLetterAtIndexFunction solution = new GetDecodeLetterAtIndexFunction();
+		
+		System.out.println("Solution: " + solution.decodeAtIndex(S, K));
 		
 	}
-	
-	public static String decodeAtIndex(String S, int K) {
-        for(int i = 0, cur = 0; i < S.length(); i++){
-            if(Character.isDigit(S.charAt(i))){
-                int number = S.charAt(i) - '0';
-                
-                System.out.println(number);
-                
-                int count = 0;
-                
-                // Doing ; since we want to use the if statement below with count
-                for(; count < number - 1 && K > cur; count++){
-                    K = K - cur;
-                }
-                
-                if(count != (number - 1)){
-                    return decodeAtIndex(S,K);
-                }else{
-                    cur = cur * number;
-                }
-            }
-            else{
-                cur++;
-                
-                K--;
-                
-                // K-- until K is zero
-                if(K == 0){
-                    return String.valueOf(S.charAt(i));
-                }
-            }
-        }
-                   return "";
-    }       
+     
 }
